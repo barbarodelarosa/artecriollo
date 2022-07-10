c6bfc0666496b9b39dc0840ca8d286ddaa63462f
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-07-10 03:02:08 -0400

    Add blog

--- blog/views.py
+++ blog/views.py
@@ -57,7 +57,7 @@ class Inicio(ListView):
             'web':obtenerWeb(),
         }
 
-        return render(request,'index_blog.html',contexto)
+        return render(request,'blog/index.html',contexto)
 
 class Listado(ListView):
 
