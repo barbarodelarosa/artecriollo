6570aff636f6812333df284d4c1351e567cdbbe6
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-07-10 19:31:14 -0400

    Add blog - categoria index

--- shop_blog/templatetags/menu_principal_tags.py
+++ shop_blog/templatetags/menu_principal_tags.py
@@ -8,7 +8,8 @@ register = template.Library()
 from django.db.models import Count
 @register.simple_tag()
 def menu_principal():
-    menu_list = Categoria.objects.annotate(count=Count('post_set__id')).order_by('count')
+    # menu_list = Categoria.objects.annotate(count=Count('post_set__id')).order_by('count')
+    menu_list = Categoria.objects.all()
     return menu_list
 
 
